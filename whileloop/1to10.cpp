31138364ad49f960622e99aaa0d357fd6c9dd7af
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-11-09 20:09:30 +0530

    Add files via upload

---
+++ whileloop/1to10.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int n = 10;
+//	printf("enter number :");
+//	scanf("%d",&n);
+	
+	int i = 1;
+	
+	while(i<=n)
+	{
+		printf("%d,",i);
+		i++;
+	}
+}
---
+++ whileloop/odd1ton.c
@@ -0,0 +1,18 @@
+//odd
+#include<stdio.h>
+main()
+{
+	int n;
+    printf("enter number :");
+    scanf("%d",&n);
+	int i = 1;
+	
+	while(i<=n)
+	{
+		if(i%2==1)
+		{
+		printf("%d,",i);
+	    }
+		i++;
+	}
+}
