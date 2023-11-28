3a1caabf375e031b84337f3e60466201ed9d7af4
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-11-28 18:11:00 +0530

    Add files via upload

---
+++ pattern/22.c
@@ -0,0 +1,19 @@
+//A
+//B C
+//D E F
+//G H I J
+//K L M N O
+#include<stdio.h>
+main()
+{
+	int i,j,a='A';
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%c ",a);
+		    a++;
+		}
+		printf("\n");
+	}
+}
