b54ba0911bc4702933a092fba3f2f99823c7f1ac
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-11-30 19:19:43 +0530

    Add files via upload

---
+++ Custom Patterns/f.c
@@ -0,0 +1,21 @@
+//F
+#include<stdio.h>
+main()
+{
+	int i,j,k;
+	for(i=1; i<=7; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			if(i==1 || i==4 || j==1)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
