4912a1b56926b15e08ca274b389279c71b95e5ac
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-12-09 16:26:35 +0530

    Update and rename boundry.c to sum boundary.c

--- 2d array/sum boundary.c
+++ 2d array/sum boundary.c
@@ -15,25 +15,25 @@ main()
           scanf("%d",&a[i][j]);
         }
 	}
+	int sum=0;
 	for(i=0; i<n; i++)
 	{
 		for(j=0; j<m; j++)
 		{
-			if(i==0||i==4)
+			if(i==0||i==4||j==4||j==0)
 			{
 				printf("%d",a[i][j]);
-			}
-			else if(j==4||j==0)
-			{
-				printf("%d",a[i][j]);
+				sum=sum+a[i][j];
 			}
 			else
 			{
 				printf(" ");
 			}
+			
 	    }
 	    printf("\n");
 	}
+	printf("\nsum of boundary :%d",sum);
 }
 //23654
 //4   5
