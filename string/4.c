b19a6b5ff6a76daf9e738667e360767fbad55886
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-12-12 19:48:22 +0530

    Update 4.c

--- string/4.c
+++ string/4.c
@@ -2,13 +2,19 @@
 #include<stdio.h>
 main()
 { 
-    int n;
-    printf("enter n :");
+    int i,n;
+    printf("enter n:");
     scanf("%d",&n);
 	char a[n];
 	printf("enter name :");	
 	scanf("%s",&a);
 
-    printf("\nsize :%d\n",sizeof a/sizeof a[n]);
-	
+    int count=0;
+    for(i=0; a[i]!=0; i++)
+    {
+    	count++;
+		
+    }
+	printf("%d",count);
+	printf("\n%d",sizeof(a)/sizeof(a[n]));	
 }
