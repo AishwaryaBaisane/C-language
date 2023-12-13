e9196fd0a5cf9a3d9c9d9f6179710a5176b00e10
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-12-13 20:47:57 +0530

    Update 7.c

--- string/7.c
+++ string/7.c
@@ -1,37 +1,31 @@
-//6. WAP to convert given string in Title Case.
+//title
 #include<stdio.h>
 #include<string.h>
-main()
+
+int main()
 {
-	char a[100];
-	
-	printf("Enter string  : ");
-	gets(a);
-	
-	int lent=strlen(a);
-	
-	if(a[0]>='a' && a[0]<='z')
-	{
-		a[0]=a[0]-32;
-	}
-	
-	int i;
-	
-	for(i=1; i<lent;i++)
-	{
-		if(a[i-1]==' ')
+   char a[100];
+   printf("enter name :");
+   gets(a);
+   int i;
+   int length=strlen(a);
+  if(a[0]>=97 && a[0]<=122)
+  {
+  	 a[0]=a[0]-32;
+  }
+  for(i=1; i<length; i++)
+  {
+  	 if(a[i-1]==' ')
 		{
-			if(a[i]>='a' && a[i]<='z')
+			if(a[i]>=97 && a[i]<=122)
 			{
 				a[i]=a[i]-32;
 			}
 		}
-		else if(a[i]>='A' && a[i]<='Z')
+		else if(a[i]>=65 && a[i]<=90)
 		{
 			a[i]=a[i]+32;
 		}
-	}
-	puts(a);
+  }
+  puts(a);
 }
-
-
