75df6fcb030cfbe8152e0be40f7ebd7773d3bfb1
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2023-12-27 15:14:57 +0530

    Add files via upload

---
+++ udfpart2/factorial.c
@@ -0,0 +1,17 @@
+//factorial
+#include<stdio.h>
+int factorial(int n)
+{
+	if(n==1)
+	{
+		return 1;
+	}
+	return n*factorial(n-1);
+}
+main()
+{
+	int n;
+	printf("enter n :");
+	scanf("%d",&n);
+	printf("%d",factorial(n));
+}
