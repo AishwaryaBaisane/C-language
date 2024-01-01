e334de8ebe73410f4f61eaf96a5405b69adb45d4
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-01-01 21:37:13 +0530

    Add files via upload

---
+++ login/gmail-id.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char a[50];
+	v:
+	printf("enter gmail :");
+	gets(a);
+	char b[10]="@gmail.com";
+	int l=strlen(a);
+	int i,check=0,j,x=0;
+	for(i=0; i<l; i++)
+	{
+		if(a[i]>=65&&a[i]<=122)
+		{
+			check=1;
+		}
+
+	}
+	if(check==1)
+	{
+	
+	for(i=0; i<l; i++)
+	{
+		if(a[i]=='@')
+		{
+			check=1; 
+			j=i;  
+		}
+	}
+	if(check==1)
+	{
+		check=0;
+	     for(i=0; i<l; i++)
+	        {
+		       if(a[i]==' ')
+		       {
+			     check=1;
+			     
+		        }
+		    }
+		    	if(check==0)
+		        {
+		         for(j;b[x]!=0;j++)
+	          	{
+		        	if(a[j]==b[x])
+		         	{
+			        	check++;
+			        }
+			       x++;
+		        }
+	               if(check==10)
+	               {
+	               	 printf("you creat gmail-id sucseesfully");
+				   }
+				   else
+				   {
+				   	 printf("keep @gmail.com together !");
+				   	 printf("\n");
+				   	 goto v;
+				   }
+				}
+				else
+				{
+					printf("space not allow !");
+					 printf("\n");
+					 goto v;
+				}
+		    
+		        
+		    }
+		    else
+		    {
+		    	printf("enter @ !");
+		    	 printf("\n");
+		    	goto v;
+			}
+ }
+ else
+ {
+ 	printf("enter valid user name !");
+ 	printf("\n");
+    goto v;
+ }
+				
+		    
+		    
+}
+	
