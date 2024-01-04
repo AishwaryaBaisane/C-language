debdef8015a0e6928cba7b990787acdd48bbbea7
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-01-04 15:21:38 +0530

    Add files via upload

---
+++ pattern/patten1.c.c
@@ -0,0 +1,28 @@
+//93571
+//9357
+//935
+//93
+//9
+#include<stdio.h>
+int main()
+{
+	int i,j=1;
+	int x=93571;
+	for(i=1; i<=5; i++)
+	{
+	    for(x; j<=i; j++)
+	    {
+	      	printf("%d",x);  
+	      
+		}
+		 x=x/10;
+		 
+		 printf("\n");
+
+	}
+     
+	    
+	   
+
+	
+}
