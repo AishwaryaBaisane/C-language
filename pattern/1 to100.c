2f9aa5351ea15b1dac305a43bb478e7314498577
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-01-05 20:25:06 +0530

    Add files via upload

---
+++ pattern/1 to100.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,k=1,y=1,l;
+	for(i=1; i<=10; i++)
+	{
+		for(l=1; l<i; l++)
+		{     
+			printf(" - ");
+		}     
+		for(j=i ;j<=10 ;j++)
+		{ 
+			printf(" %d",k);
+			k=k+j;
+    	}
+		for(j=9; j>=i; j--)
+		{
+			printf(" %d",k);
+			k=k+j;
+		}
+        y+=i;
+		k=i+y;
+		printf("\n");     
+    }
+}
