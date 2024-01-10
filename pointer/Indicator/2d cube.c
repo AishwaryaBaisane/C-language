463116268f5cf547f787c615b05d1bd46a47399f
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-01-10 15:35:05 +0530

    Add files via upload

---
+++ pointer/Indicator/2d cube.c
@@ -0,0 +1,39 @@
+//2d array find cube
+#include<stdio.h>
+#include<string.h>
+
+void cube(int n)
+{
+	int a[n][n];
+	int i,j;
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+		 printf("enter array a[%d][%d]",i,j);
+		 scanf("%d",&a[i][j]);
+		}
+		
+   }
+
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+		    a[i][j] = a[i][j] * a[i][j]* a[i][j];
+	     	printf("%d ",a[i][j]);	
+		}
+	  	
+	}
+}
+
+int main()
+{
+	int n;
+	printf("enter size of array : ");
+	scanf("%d",&n);
+
+
+
+	cube(n);
+}
